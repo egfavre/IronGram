f83c856c9610d414256a489c045f2b3272ee3cbe
Liz Favre <egfavre@gmail.com>
2016-06-28 12:31:35 -0400

    I have no idea what we did but it worked.

--- src/main/java/com/egfavre/controllers/IronGramController.java
+++ src/main/java/com/egfavre/controllers/IronGramController.java
@@ -1,12 +1,21 @@
 package com.egfavre.controllers;
 
+import com.egfavre.entities.Photo;
+import com.egfavre.entities.User;
 import com.egfavre.services.PhotoRepository;
 import com.egfavre.services.UserRepository;
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.sql.SQLException;
 
 /**
@@ -24,4 +33,28 @@ public class IronGramController {
     public void init() throws SQLException {
         Server.createWebServer().start();
     }
+
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String upload (MultipartFile file, String receiver, HttpSession session) throws Exception {
+        String username = (String) session.getAttribute("username");
+        User sender = users.findFirstByName(username);
+        User rec = users.findFirstByName(receiver);
+
+        if (sender == null || rec==null){
+            throw new Exception("Can't find sender or receiver");
+        }
+        File dir = new File("public/photos");
+        dir.mkdirs();
+
+        File photoFile = File.createTempFile("photo", file.getOriginalFilename(), dir);
+
+        FileOutputStream fos = new FileOutputStream(photoFile);
+        fos.write(file.getBytes());
+
+        Photo photo = new Photo(sender, rec, photoFile.getName());
+        photos.save(photo);
+
+        return "redirect:/";
+    }
+
 }
--- src/main/java/com/egfavre/services/PhotoRepository.java
+++ src/main/java/com/egfavre/services/PhotoRepository.java
@@ -1,10 +1,12 @@
 package com.egfavre.services;
 
 import com.egfavre.entities.Photo;
+import com.egfavre.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by user on 6/28/16.
  */
 public interface PhotoRepository extends CrudRepository<Photo, Integer> {
+    public Iterable<Photo> findByRecipient(User recipient);
 }
