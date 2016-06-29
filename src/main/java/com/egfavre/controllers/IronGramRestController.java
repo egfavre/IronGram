e2e41602e252ca8d0fe514a08fbee0ca02ca56fb
Liz Favre <egfavre@gmail.com>
2016-06-29 10:14:07 -0400

    add logout during class

--- src/main/java/com/egfavre/controllers/IronGramRestController.java
+++ src/main/java/com/egfavre/controllers/IronGramRestController.java
@@ -93,4 +93,15 @@ public class IronGramRestController {
             }
         return fullList;
     }
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public User getUser(HttpSession session){
+        String username = (String) session.getAttribute("username");
+        if (username == null){
+            return null;
+        }
+        else {
+            return  users.findFirstByName(username);
+        }
+    }
 }
